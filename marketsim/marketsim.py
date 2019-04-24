4b103402d32c36d6c715692d6fb86a7ce9f06b59
lostleaf <qinglin@limao.tech>
2019-04-24 17:29:08 +0800

    Finish marketsim

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -1,91 +1,95 @@
-"""MC2-P1: Market simulator.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			    		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		   	  			    		  		  		    	 		 		   		 		  
-All Rights Reserved  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Template code for CS 4646/7646  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			    		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		   	  			    		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		   	  			    		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		   	  			    		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		   	  			    		  		  		    	 		 		   		 		  
-or edited.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-We do grant permission to share solutions privately with non-students such  		   	  			    		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		   	  			    		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		   	  			    		  		  		    	 		 		   		 		  
-GT honor code violation.  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
------do not edit anything above this line---  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-Student Name: Tucker Balch (replace with your name)  		   	  			    		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		   	  			    		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		   	  			    		  		  		    	 		 		   		 		  
-"""  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-import pandas as pd  		   	  			    		  		  		    	 		 		   		 		  
-import numpy as np  		   	  			    		  		  		    	 		 		   		 		  
-import datetime as dt  		   	  			    		  		  		    	 		 		   		 		  
-import os  		   	  			    		  		  		    	 		 		   		 		  
-from util import get_data, plot_data  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, commission=9.95, impact=0.005):  		   	  			    		  		  		    	 		 		   		 		  
-    # this is the function the autograder will call to test your code  		   	  			    		  		  		    	 		 		   		 		  
-    # NOTE: orders_file may be a string, or it may be a file object. Your  		   	  			    		  		  		    	 		 		   		 		  
-    # code should work correctly with either input  		   	  			    		  		  		    	 		 		   		 		  
-    # TODO: Your code here  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    # In the template, instead of computing the value of the portfolio, we just  		   	  			    		  		  		    	 		 		   		 		  
-    # read in the value of IBM over 6 months  		   	  			    		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008,1,1)  		   	  			    		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008,6,1)  		   	  			    		  		  		    	 		 		   		 		  
-    portvals = get_data(['IBM'], pd.date_range(start_date, end_date))  		   	  			    		  		  		    	 		 		   		 		  
-    portvals = portvals[['IBM']]  # remove SPY  		   	  			    		  		  		    	 		 		   		 		  
-    rv = pd.DataFrame(index=portvals.index, data=portvals.as_matrix())  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    return rv  		   	  			    		  		  		    	 		 		   		 		  
-    return portvals  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-def test_code():  		   	  			    		  		  		    	 		 		   		 		  
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import os
+from util import get_data, plot_data
+
+
+def author():
+    return 'lostleaf'
+
+
+def compute_portvals(orders_file="./orders/orders.csv", start_val=1000000, commission=9.95, impact=0.005):
+    def trade_cost(x):
+        if not x['Symbol']:
+            return 0
+        return -(x[x['Symbol']] * x['Order'] * x['Shares']) * 1 - impact * x[x['Symbol']] * x['Shares'] - commission
+
+    orders = pd.read_csv(orders_file)
+
+    orders.loc[orders['Order'] == 'BUY', 'Order'] = 1
+    orders.loc[orders['Order'] == 'SELL', 'Order'] = -1
+    orders['Order'] = pd.to_numeric(orders['Order'])
+    orders['Date'] = pd.to_datetime(orders['Date'])
+    orders.set_index('Date', inplace=True)
+
+    start_date = orders.index.min()
+    end_date = orders.index.max()
+    symbols = orders['Symbol'].unique()
+
+    market_data = get_data(symbols, pd.date_range(start_date, end_date))
+    tmp = market_data.join(orders).fillna(0)
+    cash_delta = tmp.apply(trade_cost, axis=1).groupby(level=0).sum()
+    cash = start_val + cash_delta.cumsum()
+
+    pos_delta = pd.DataFrame(index=market_data.index)
+    for symbol, df in orders.groupby('Symbol'):
+        pos_delta[symbol] = (df['Order'] * df['Shares']).groupby(level=0).sum()
+    pos = pos_delta.fillna(0).cumsum()
+    port_val = (pos * market_data[symbols]).sum(axis=1) + cash
+
+    return port_val
+
+
+def evaluate_portfolio(port_val):
+    cr = port_val.iloc[-1] / port_val.iloc[0]
+    dr = (port_val / port_val.shift(1) - 1).dropna()
+    adr = dr.mean()
+    sddr = dr.std()
+    sr = (adr) / sddr * np.sqrt(252)
+    return cr, adr, sddr, sr
+
+
+def test_code():
     # this is a helper function you can use to test your code  		   	  			    		  		  		    	 		 		   		 		  
     # note that during autograding his function will not be called.  		   	  			    		  		  		    	 		 		   		 		  
     # Define input parameters  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    of = "./orders/orders2.csv"  		   	  			    		  		  		    	 		 		   		 		  
-    sv = 1000000  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
+
+    of = "./orders/orders-02.csv"
+    sv = 1000000
+
     # Process orders  		   	  			    		  		  		    	 		 		   		 		  
-    portvals = compute_portvals(orders_file = of, start_val = sv)  		   	  			    		  		  		    	 		 		   		 		  
-    if isinstance(portvals, pd.DataFrame):  		   	  			    		  		  		    	 		 		   		 		  
-        portvals = portvals[portvals.columns[0]] # just get the first column  		   	  			    		  		  		    	 		 		   		 		  
-    else:  		   	  			    		  		  		    	 		 		   		 		  
-        "warning, code did not return a DataFrame"  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    # Get portfolio stats  		   	  			    		  		  		    	 		 		   		 		  
+    portvals = compute_portvals(orders_file=of, start_val=sv)
+    if isinstance(portvals, pd.DataFrame):
+        portvals = portvals[portvals.columns[0]]
+    else:
+        "warning, code did not return a DataFrame"
+
+        # Get portfolio stats
     # Here we just fake the data. you should use your code from previous assignments.  		   	  			    		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008,1,1)  		   	  			    		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008,6,1)  		   	  			    		  		  		    	 		 		   		 		  
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [0.2,0.01,0.02,1.5]  		   	  			    		  		  		    	 		 		   		 		  
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [0.2,0.01,0.02,1.5]  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-    # Compare portfolio against $SPX  		   	  			    		  		  		    	 		 		   		 		  
-    print "Date Range: {} to {}".format(start_date, end_date)  		   	  			    		  		  		    	 		 		   		 		  
-    print  		   	  			    		  		  		    	 		 		   		 		  
-    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)  		   	  			    		  		  		    	 		 		   		 		  
-    print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)  		   	  			    		  		  		    	 		 		   		 		  
-    print  		   	  			    		  		  		    	 		 		   		 		  
-    print "Cumulative Return of Fund: {}".format(cum_ret)  		   	  			    		  		  		    	 		 		   		 		  
-    print "Cumulative Return of SPY : {}".format(cum_ret_SPY)  		   	  			    		  		  		    	 		 		   		 		  
-    print  		   	  			    		  		  		    	 		 		   		 		  
-    print "Standard Deviation of Fund: {}".format(std_daily_ret)  		   	  			    		  		  		    	 		 		   		 		  
-    print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)  		   	  			    		  		  		    	 		 		   		 		  
-    print  		   	  			    		  		  		    	 		 		   		 		  
-    print "Average Daily Return of Fund: {}".format(avg_daily_ret)  		   	  			    		  		  		    	 		 		   		 		  
-    print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)  		   	  			    		  		  		    	 		 		   		 		  
-    print  		   	  			    		  		  		    	 		 		   		 		  
-    print "Final Portfolio Value: {}".format(portvals[-1])  		   	  			    		  		  		    	 		 		   		 		  
-  		   	  			    		  		  		    	 		 		   		 		  
-if __name__ == "__main__":  		   	  			    		  		  		    	 		 		   		 		  
-    test_code()  		   	  			    		  		  		    	 		 		   		 		  
+    start_date = datetime(2011, 1, 5)
+    end_date = datetime(2011, 1, 20)
+    data_SPY = get_data(['SPY'], pd.date_range(start_date, end_date))
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = evaluate_portfolio(portvals)
+    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = evaluate_portfolio(data_SPY)
+
+    # Compare portfolio against $SPX
+    print "Date Range: {} to {}".format(start_date, end_date)
+    print
+    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
+    # print "Sharpe Ratio of SPY : {}".format(sharpe_ratio_SPY)
+    print
+    print "Cumulative Return of Fund: {}".format(cum_ret)
+    # print "Cumulative Return of SPY : {}".format(cum_ret_SPY)
+    print
+    print "Standard Deviation of Fund: {}".format(std_daily_ret)
+    # print "Standard Deviation of SPY : {}".format(std_daily_ret_SPY)
+    print
+    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
+    # print "Average Daily Return of SPY : {}".format(avg_daily_ret_SPY)
+    print
+    print "Final Portfolio Value: {}".format(portvals[-1])
+
+
+if __name__ == "__main__":
+    test_code()
